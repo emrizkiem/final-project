b75f8d146852f506195ec7a8cb675ae88f27b40c
Agung Firmansyah <agungfirid@gmail.com>
2021-09-11 22:16:11 +0700

    updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,12 @@
-import {
-    BrowserRouter,
-    Route,
-    Switch
-  } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-  import { Component } from 'react';
-  import Login from "../pages/login"
-  import Dashboard from "../pages/dashboard"
-  import generalDashboard from 'pages/general_support';
-  import DashboardPayment from 'pages/dashboard/dashboard_rada';
-
+import Login from '../pages/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login} />   
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/general_support" component={generalDashboard} />
-        <Route path="/DashboardPayment" component={DashboardPayment} />
-    
+        <Route exact path="/" component={Login} />
       </Switch>
     </BrowserRouter>
   );
